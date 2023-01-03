c21a8d22ad24ad15573175e1ead8c2842605da03
zuies <zurabichkhikvadze66@gmail.com>
2023-01-03 09:51:13 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#0081C9'
+      }
+    },
+  },
+  plugins: [],
+}
