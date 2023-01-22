2417853ddb0b6c99cf4a2abde79d2abd7b7882b2
zuies <zurabichkhikvadze66@gmail.com>
2023-01-22 15:57:39 +0400

    change component name

--- pages/our-vision.vue
+++ pages/our-vision.vue
@@ -12,7 +12,7 @@
 
 <script setup>
 import RowFive from "~~/components/pages/homepage/RowFive.vue";
-import MainSection from "~/components/pages/ourVision/MainSection.vue";
+import MainSection from "~/components/pages/ourVision/mainSection.vue";
 import RowOne from "~~/components/pages/ourVision/RowOne.vue";
 useHead({
   titleTemplate: "%s - ToghetherCrew",
