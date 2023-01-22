cf422bc3659a887f131261553e7a9b46d429c123
zuies <zurabichkhikvadze66@gmail.com>
2023-01-22 15:07:19 +0400

    make landing static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    "/pages/**": { static: true },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
