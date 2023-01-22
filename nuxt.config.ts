4655932c277ff92ea14e2060fc63dec90809c099
zuies <zurabichkhikvadze66@gmail.com>
2023-01-22 17:28:53 +0400

    remove ssr in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   postcss: {
     plugins: {
       tailwindcss: {},
