996f7548d7527f53bf5320aa329710380874a377
zuies <zurabichkhikvadze66@gmail.com>
2023-01-22 15:33:39 +0400

    update package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    "/pages/**": { static: true },
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
