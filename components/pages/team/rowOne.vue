4cde311a604b04b5979743689f9a95f67c4c813d
zuies <zurabichkhikvadze66@gmail.com>
2023-02-02 18:38:25 +0400

    change rowOne in team

--- components/pages/team/rowOne.vue
+++ components/pages/team/rowOne.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="space-y-4 mt-4 md:mt-0 pt-12">
-    <h1 class="text-4xl font-bold text-center">Meet our team</h1>
-    <p class="text-base text-center">
-      We are an international <b>DAO team</b> of data <br />
-      scientists and Web3 enthusiasts
-    </p>
+    <div class="md:mb-12 space-y-4">
+      <h1 class="text-4xl font-bold text-center">Meet our team</h1>
+      <p class="text-base text-center">
+        We are an international <b>DAO team</b> of data <br />
+        scientists and Web3 enthusiasts
+      </p>
+    </div>
     <div
       class="bg-white shadow-2xl p-12 gap-4 m-3 md:m-0 rounded-box flex flex-row flex-wrap justify-center"
     >
