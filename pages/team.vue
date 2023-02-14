4b22ffb19d9e470345fb23f50a86be6e3f282e83
zuies <zurabichkhikvadze66@gmail.com>
2023-02-14 17:32:19 +0400

    update notion

--- pages/team.vue
+++ pages/team.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <row-one />
     </div>
-    <footer-section class="mt-12"/>
+    <footer-section class="mt-12" />
   </div>
 </template>
 
@@ -15,6 +15,26 @@ useHead({
   titleTemplate: () => {
     return "Team | ToghetherCrew";
   },
+});
+const team = ref("");
+onMounted(async () => {
+  const { data: weatherdata, pending } = await useAsyncData("weatherdata", () =>
+    $fetch(
+      `https://api.notion.com/v1/databases/fb857b3358d74559a8fa70528a39dec6/query`,
+      {
+        method: "post",
+        headers: {
+          Authorization: "secret_aMZqZwPB648QMjue9STo3aAEx1LahPALmo7iis1kX9G",
+          "Notion-Version": "2022-06-28",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+        },
+      }
+    )
+  ).catch((e) => {
+    console.log(e);
+  });
 });
 </script>
 
